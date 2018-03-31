34fb8eeb81cfb5849c6ed8cff061b7ff1ef3b698
MikaGuraN <1069952275@qq.com>
2018-03-31 14:01:35 +0800

    更新更新更

--- 后端/基本/src/main/java/间/安卓/后端/用户.java
+++ 后端/基本/src/main/java/间/安卓/后端/用户.java
@@ -20,9 +20,14 @@ import 间.安卓.工具.线程;
 import android.widget.ImageView;
 import 间.安卓.视图.图片视图;
 import 间.安卓.视图.实现.图片实现;
+import 间.安卓.工具.处理;
 
 public class 用户 extends AVUser {
 
+    static {
+        AVUser.alwaysUseSubUserClass(用户.class);
+    }
+    
     public 用户() {
         super();
     }
@@ -50,47 +55,53 @@ public class 用户 extends AVUser {
     public void 置头像(Bitmap $头像) {
         置文件("icon", 图片.转换($头像));
     }
-    
+
     public void 置头像(String $头像) {
         置文件("icon", $头像);
     }
-    
+
     public void 取头像(final 方法 $回调) {
         new 线程(new 方法() {
                 @Override
                 public Object 调用(Object[] $参数) {
-                    调用.事件($回调,同步取头像());
+                    调用.事件($回调, 同步取头像());
                     return null;
                 }
             }).启动();
     }
-    
+
     public void 显示头像(ImageView $图片视图) {
-        显示头像($图片视图,null);
+        显示头像($图片视图, null);
     }
-    
+
     public void 显示头像(final ImageView $图片视图,final Object $默认) {
         new 线程(new 方法() {
                 @Override
                 public Object 调用(Object[] $参数) {
-                    返回值<Bitmap> $头像 = 同步取头像();
-                    if ($头像.成功()) {
-                        图片实现.置图片($图片视图,$头像.取内容());
-                    } else if($默认 != null) {
-                        图片实现.置图片($图片视图,$默认);
-                    }
+                    final 返回值<Bitmap> $头像 = 同步取头像();
+                    处理.主线程(new 方法() {
+                            @Override
+                            public Object 调用(Object[] $参数) {
+                                if ($头像.成功()) {
+                                    图片实现.置图片($图片视图, $头像.取内容());
+                                } else if ($默认 != null) {
+                                    图片实现.置图片($图片视图, $默认);
+                                }
+                                return null;
+                            }
+                        });
                     return null;
                 }
             }).启动();
     }
-    
-    
+
+
     public 返回值<Bitmap> 同步取头像() {
         返回值<InputStream> $返回 = 取文件("icon");
         if ($返回.成功()) {
             return 返回值.创建(图片.读取($返回.取内容()));
         } else {
-            return 返回值.创建(null,$返回.取错误());
+            return 返回值.创建(null, $返回.取错误());
         }
     }
 
@@ -128,20 +139,15 @@ public class 用户 extends AVUser {
                 }
             });
     }
-    
+
     public boolean 检查() {
         return isAuthenticated();
     }
 
     public void 检查(final 方法 $回调) {
-        isAuthenticated(new AVCallback<Boolean>() {
-                @Override
-                protected void internalDone0(Boolean $结果,后端错误 $错误) {
-                    调用.事件($回调,返回值.创建(null,$结果,$错误));
-                }
-            });
+        登录(getSessionToken(), $回调);
     }
-    
+
     public static void 登出() {
         logOut();
     }
@@ -152,6 +158,24 @@ public class 用户 extends AVUser {
         } catch (后端错误 $错误) {
             return 返回值.创建(null, $错误);
         }
+    }
+
+    public static 返回值<用户> 同步登录(String $Token) {
+        try {
+            return 返回值.创建((用户)becomeWithSessionToken($Token, 用户.class));
+        } catch (后端错误 $错误) {
+            return 返回值.创建(null, $错误);
+        }
+    }
+
+
+    public static void 登录(String $Token,final 方法 $回调) {
+        becomeWithSessionTokenInBackground($Token, new LogInCallback<用户>() {
+                @Override
+                public void done(用户 $用户,后端错误 $错误) {
+                    调用.事件($回调, 返回值.创建($用户, $错误));
+                }
+            }, 用户.class);
     }
 
     public static void 登录(String $账号,String $密码,final 方法 $回调) {
