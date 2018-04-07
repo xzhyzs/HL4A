45993da76d9f9e540cf228c0a2ebb09d6e7e5185
MikaGuraN <1069952275@qq.com>
2018-04-07 20:17:52 +0800

    更新更新更

--- 安卓/src/main/java/间/安卓/工具/线程.java
+++ 安卓/src/main/java/间/安卓/工具/线程.java
@@ -5,8 +5,6 @@ import 间.接口.*;
 
 public class 线程 extends 间.工具.线程 {
     
-    public Looper L;
-    
     public 线程(方法 $方法) {
         super($方法);
     }
@@ -14,21 +12,9 @@ public class 线程 extends 间.工具.线程 {
     public 线程(Object $对象,String $方法名) {
         super($对象,$方法名);
     }
-
-    @Override
-    public void run() {
-        Looper.prepare();
-        L = Looper.myLooper();
-        super.run();
-        Looper.loop();
-    }
-    
-    public void 处理(方法 $方法,Object... $参数) {
-        处理.指定(L,$方法,$参数);
-    }
     
     public static boolean 是主线程() {
-        return Looper.myLooper() == Looper.getMainLooper();
+        return Looper.getMainLooper().getThread() == 取当前线程();
     }
     
 }
